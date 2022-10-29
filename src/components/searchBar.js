c96531bb003a3c847463af08e25194af9a1dde0e
EliasSahlos <esahlos12@gmail.com>
2022-10-29 16:42:55 +0300

    TOURGUIDE v0.1 Added Search-bar functionality Added API Call from Back-End

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -3,84 +3,103 @@ import TextField from "@mui/material/TextField";
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import {Autocomplete} from "@mui/material";
+import Button from '@mui/material/Button';
+import axios from "axios";
 
 
+const cities = [
+    "Paris",
+    "London",
+    "New York",
+    "Tokyo",
+    "Berlin",
+    "Buenos Aires",
+    "Cairo",
+    "Canberra",
+    "Rio de Janeiro",
+    "Dublin"
+];
 
-export default function SearchBarComp(changeSearchQuery, ...props) {
+
+const filterData = (query) => {
+    if (!query) {
+        return cities;
+    } else {
+        return cities.filter((d) => d.toLowerCase().includes(query));
+    }
+};
+
+const SearchBar = ({searchQuery, setSearchQuery, onFormSubmit}) => (
+    <form onSubmit={onFormSubmit}>
+        <Autocomplete
+            onInputChange={(e) => setSearchQuery(e)}
+            disableClearable={true}
+            value={searchQuery}
+            options={cities}
+            renderInput={(params) =>
+                <TextField
+                    {...params}
+                    id="search-bar"
+                    className="text"
+                    onInput={(e) => {
+                        setSearchQuery(e.target.value)
+                    }}
+                    value={searchQuery}
+                    label="Enter A City Name"
+                    variant="filled"
+                    placeholder="Search..."
+                    size="small"
+                    style={{width: 600, backgroundColor: "white", borderRadius: "5px", fontFamily: "ubuntu"}}
+                />}
+
+        />
+    </form>
+);
+
+export default function SearchBarComp(props) {
     const [value, setValue] = useState(null)
+    const [imagesSerialized, setImagesSerialized] = useState([])
     const [searchQuery, setSearchQuery] = useState("");
     const dataFiltered = filterData(searchQuery);
 
-    const onSearchSubmit = (e) => {
-        if (e.key === "Enter") {
-            changeSearchQuery.call(this, searchQuery)
-        }
+    const handleChangeWord = (e) => {
+        setSearchQuery(e.target.value)
+
     }
 
-    const cities = [
-        "Paris",
-        "London",
-        "New York",
-        "Tokyo",
-        "Berlin",
-        "Buenos Aires",
-        "Cairo",
-        "Canberra",
-        "Rio de Janeiro",
-        "Dublin",
-        "Jakarta"
-    ];
-
-    const filterData = (query) => {
-        if (!query) {
-            return cities;
-        } else {
-            return cities.filter((d) => d.toLowerCase().includes(query));
-        }
-    };
-
-    const SearchBar = ({setSearchQuery}) => (
-        <form>
-            <Autocomplete
-                options={cities}
-                renderInput={(params) =>
-                    <TextField
-                        {...params}
-                        id="search-bar"
-                        className="text"
-                        /*onInput={(e) => {
-                            setSearchQuery(e.target.value);
-                        }}*/
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        onKeyDown={onSearchSubmit}
-                        label="Enter A City Name"
-                        variant="filled"
-                        placeholder="Search..."
-                        size="small"
-                        style={{width: 800, backgroundColor: "white", borderRadius: "5px"}}
-                    />}
-
-            />
-        </form>
-    );
+    const handleSubmitForm = (e) => {
+        e.preventDefault()
+        props.changeWord(searchQuery)
+        props.submitForm(e)
+    }
 
     return (
         <Box>
 
-            <div
-                style={{
-                    alignItems: "center",
-                    display: "flex",
-                    alignSelf: "center",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    padding: 20
-                }}
-            >
-                <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
-                {/*{console.log(searchQuery)}*/}
+            <div>
+                <div
+                    style={{
+                        alignItems: "center",
+                        display: "flex",
+                        alignSelf: "center",
+                        justifyContent: "center",
+                        padding: "20px"
+                    }}
+                >
+                    <SearchBar searchQuery={searchQuery} setSearchQuery={handleChangeWord}
+                               onFormSubmit={handleSubmitForm}/>
+                    <Button type="submit" variant="contained" onClick={handleSubmitForm}
+                            style={{
+                                borderRadius: "8px",
+                                height: "49px",
+                                backgroundColor: "#0E1822",
+                                fontFamily: "ubuntu"
+                            }}>
+                        Search City
+                    </Button>
+                </div>
             </div>
+
         </Box>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,73 +1,40 @@
 import "../style/Home.css"
 import SearchBarComp from "../components/searchBar";
-import CityInfoHeader from "../components/City-info-Header";
-import {useEffect, useState} from "react";
+import CityInfoHeader from "../components/CityInfoHeader";
+import {useState} from "react";
+import axios from "axios";
 
 function Home() {
+    const [cityProp, setCityProp] = useState("")  //City Name From Search Bar
+    const [cityData, setCityData] = useState([])
 
-    const [imagesSerialized, setImagesSerialized] = useState([])
-    const [firstLoad, setFirstLoad] = useState(true)
-
-
-    function getAllImages(query){                                               //ayto to kaloyme apo to search bar dinontas toy to swsto Query
-        const axios = require('axios')
-        let config = {
-            method: 'get',
-            url: "localhost:8080/api/v1/images/getByTitle/" + query,
-            headers: {
-
-            }
-        }
-
-        axios(config)
-            .then(function (response){
-                const allResponse = response.data
-                serializeImages(allResponse)
-            })
-            .catch(function (error){
-                console.log(error);
-            })
+    function getAllImages() {
+        let APICallString = "http://localhost:8080/api/v1/images/getByTitle/Thessaloniki";
+        axios.get(APICallString).then(function (response) {
+            const data = response.data
+            setCityData(data)
+            console.log(data)
+        }).catch(function (error) {
+            console.log(error)
+        })
     }
 
-    function serializeImages(response){
-        let imagesArray = []
-        for(let i = 0; i<response['images'].length; i++){                      //otidhpote edw gia na metrhsw to length
-
-            let imageObj = {
-                id: response['images'][i]['id'],
-                title: response['images'][i]['title'],
-                description: response['images'][i]['description']
-            }
-            imagesArray.push(imageObj)
-        }
-        setImagesSerialized(imagesArray)
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        getAllImages()
     }
-
-
-    useEffect(() => {
-        if (typeof window !== "undefined"){
-            if(firstLoad){
-                getAllImages("Thessaloniki")
-                setFirstLoad(false)
-                console.log(imagesSerialized)
-            }
-        }
-    }, [firstLoad, getAllImages, imagesSerialized])
-
-
-
 
     return (
+
         <div className="Home">
             <div className="search-container">
                 <div className="searchbar">
-                    <SearchBarComp
-                        changeSearchQuery={getAllImages}
-                    />
+                    {/*<button onClick={e => getAllImages()}>Click me</button>*/}
+                    <SearchBarComp changeWord={word => setCityProp(word)} submitForm={handleFormSubmit}/>
                 </div>
             </div>
             <div className="city-container">
-                <CityInfoHeader/>
+                <CityInfoHeader cityProp={cityProp} cityData={cityData} />
             </div>
         </div>
     );
