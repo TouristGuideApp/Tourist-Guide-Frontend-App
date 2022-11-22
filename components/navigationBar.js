d9a5a46d762551bc489554720cdc8d14f2d9808a
AlexaderLoco <alexlocopap@gmail.com>
2022-11-22 20:37:53 +0200

    Fixed some of the logic of how the navbar works.

--- components/navigationBar.js
+++ components/navigationBar.js
@@ -11,12 +11,11 @@ import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-
-const settings = ["Log In", "Sign In"];
+import {useState} from "react";
 
 function NavigationBar() {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
@@ -33,13 +32,15 @@ function NavigationBar() {
         setAnchorElUser(null);
     };
 
-    return (
-        <AppBar position="static">
+    const settings = ["Log In", "Sign Up"];
 
-            <Container maxWidth="string" style={{backgroundColor: "rgba(0,0,0,0.2)", zIndex: "999"}}>
+    return (
+        <AppBar position="static" style={{backgroundColor:"transparent", border: "none", boxShadow: "none"}}>
+
+            <Container maxWidth="string">
                 <Toolbar disableGutters>
                     <AdbIcon
-                        sx={{display: {xs: "none", md: "flex"}, mr: 1}}
+                        sx={{display: {xs: "none", md: "flex"}, mr: 1, color: "white", zIndex: 2}}
                     />
 
                     <Typography
@@ -53,79 +54,21 @@ function NavigationBar() {
                             fontWeight: 700,
                             letterSpacing: ".1rem",
                             fontFamily: "Ubuntu",
-                            color: "inherit",
                             textDecoration: "none",
+                            color: "white",
+                            zIndex: 2
                         }}
                     >
                         TourGuide
-                    </Typography>
-
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: {xs: "flex", md: "none"},
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon/>
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: {xs: "block", md: "none"},
-                            }}
-                        >
-                        </Menu>
-                    </Box>
-                    <AdbIcon
-                        sx={{display: {xs: "flex", md: "none"}, mr: 1}}
-                    />
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: {xs: "flex", md: "none"},
-                            flexGrow: 1,
-                            fontFamily: "monospace",
-                            fontWeight: 700,
-                            letterSpacing: ".3rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        LOGO
                     </Typography>
                     <Box
                         sx={{
                             flexGrow: 1,
                             display: {xs: "none", md: "flex"},
+                            color: 'white', zIndex: 2
                         }}
-                    >
-
-                    </Box>
-                    <Box sx={{flexGrow: 0}}>
+                    />
+                    <Box sx={{flexGrow: 0, color: 'white', zIndex: 2}}>
                         <Tooltip title="Open settings">
                             <IconButton
                                 onClick={handleOpenUserMenu}
