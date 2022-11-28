a2c84dce8cd8d6df56960c1379116c327ab40d62
EliasSah <esahlos12@gmail.com>
2022-11-28 13:14:39 +0200

    Center Aligned pagination component.

--- components/cityInfoHeader.js
+++ components/cityInfoHeader.js
@@ -1,70 +1,68 @@
 import * as React from "react";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
-import {Stack, Tooltip} from "@mui/material";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import {Pagination} from "@mui/lab";
-import {useState} from 'react';
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from "@mui/icons-material/Info";
+import { Stack, Tooltip } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { Pagination } from "@mui/lab";
+import { useState } from "react";
 import Link from "next/link";
 
-
-function CityInfoHeader({mapInfo, cityProp, maxPage, currentPage, changeCurrentPage}) {
-
+function CityInfoHeader({ mapInfo, cityProp, maxPage, currentPage, changeCurrentPage }) {
     const [page, setPage] = useState(currentPage);
 
     const handlePageChange = (event, newPage) => {
         setPage(newPage);
-        changeCurrentPage.call(this, newPage )
+        changeCurrentPage.call(this, newPage);
     };
     return (
         <>
             <div className="city-name">
                 <h1>{cityProp.toUpperCase()}</h1>
-                <hr/>
+                <hr />
             </div>
-            <Pagination count={maxPage} onChange={handlePageChange} page={page} />
-            <ImageList sx={{width: "100%", height: "100%"}}>
-                <ImageListItem key="Subheader" cols={4}>
-                </ImageListItem>
+            <Stack alignItems="center">
+                <Pagination count={maxPage} onChange={handlePageChange} page={page}/>
+            </Stack>
+            <ImageList sx={{ width: "100%", height: "100%" }}>
+                <ImageListItem key="Subheader" cols={4}></ImageListItem>
                 {mapInfo.content.map((cityObj) => {
                     return (
                         <>
                             <ImageListItem key={cityObj.img}>
                                 <img
-                                    style={{width: "100%", height: "350px"}}
+                                    style={{ width: "100%", height: "350px" }}
                                     src={cityObj.fileName}
                                     alt="Image Could Not be Load"
                                     loading={"lazy"}
                                 />
                                 <ImageListItemBar
-                                    sx={{fontFamily: "Ubuntu"}}
+                                    sx={{ fontFamily: "Ubuntu" }}
                                     title={cityObj.title}
                                     subtitle={"Views : " + cityObj.views}
                                     actionIcon={
                                         <IconButton
-                                            sx={{color: 'rgba(255, 255, 255, 0.6)'}}
+                                            sx={{ color: "rgba(255, 255, 255, 0.6)" }}
                                             aria-label={`info about ${cityObj.title}`}
                                         >
                                             <Tooltip title={"Total Views: " + cityObj.views} placement="top" arrow>
-                                                <VisibilityIcon style={{marginRight: "10px"}}/>
+                                                <VisibilityIcon style={{ marginRight: "10px" }} />
                                             </Tooltip>
                                             <Link href={"/image/" + cityObj.id}>
-                                                <InfoIcon/>
+                                                <InfoIcon />
                                             </Link>
                                         </IconButton>
                                     }
                                 />
                             </ImageListItem>
                         </>
-                    )
+                    );
                 })}
             </ImageList>
-
         </>
     );
 }
 
-export default CityInfoHeader
+export default CityInfoHeader;
