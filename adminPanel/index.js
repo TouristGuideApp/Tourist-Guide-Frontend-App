fb0781405a1e2e73fe541eda0c5af046597ff774
7ermi <7erminatrontzou7@gmail.com>
2022-12-01 19:56:24 +0200

    Converted it to adminPanel directory instead

--- adminPanel/index.js
+++ adminPanel/index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Admin = () => (
     <div>
-        <h1 className="title is-1">This is the admin Page</h1>
+        <h1>This is the admin Page</h1>
         <p>
             Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
             inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
