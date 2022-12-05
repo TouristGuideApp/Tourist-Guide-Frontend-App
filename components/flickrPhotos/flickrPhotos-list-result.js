67add87b1f85e62f65e8ddfe374b2dd5562448cd
7ermi <7erminatrontzou7@gmail.com>
2022-12-05 10:43:53 +0200

    Removed list-toolbar

--- components/flickrPhotos/flickrPhotos-list-result.js
+++ components/flickrPhotos/flickrPhotos-list-result.js
@@ -1,15 +1,14 @@
 import * as React from "react";
 
 function FlickrPhotosListResult({cityProp, cityData}){
-    console.log(cityData)
     return(
         <>
             {cityData.map((imgObj) => {
                 return (
                     <>
                         <div className="city-name">
-                            <p>{imgObj.title}</p>
-                            <hr />
+                            <p>{imgObj.views}</p>
+
                         </div>
                     </>
                 )
