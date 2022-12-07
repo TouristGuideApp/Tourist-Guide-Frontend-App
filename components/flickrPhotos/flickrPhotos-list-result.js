dd9dae8883c57d4fead5d53f7be4439cdb198113
matsaki95 <matsaki95@tutanota.com>
2022-12-08 01:31:50 +0200

    Made the initial version of the table that will show the results of the API call

--- components/flickrPhotos/flickrPhotos-list-result.js
+++ components/flickrPhotos/flickrPhotos-list-result.js
@@ -1,19 +1,73 @@
 import * as React from "react";
+import {Table, TableCell, TableHead, TableRow} from "@mui/material";
 
-function FlickrPhotosListResult({cityProp, cityData}){
-    return(
+const maxRowHeight = "100px"
+
+function FlickrPhotosListResult({cityProp, cityData}) {
+    return (
         <>
-            {cityData.map((imgObj) => {
-                return (
-                    <>
-                        <div className="city-name">
-                            <p>{imgObj.views}</p>
+            <div className="city-name">
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell> Flickr id </TableCell>
+                            <TableCell> Image </TableCell>
+                            <TableCell> Title </TableCell>
+                            <TableCell> Description </TableCell>
+                            <TableCell> Date taken </TableCell>
+                            <TableCell> Owner </TableCell>
+                            <TableCell> Latitude </TableCell>
+                            <TableCell> Longitude </TableCell>
+                            <TableCell> Views </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    {cityData.map((imgObj) => {
+                        return (
+                            <>
 
-                        </div>
-                    </>
-                )
-            })}
+                                <TableRow>
+                                    <TableCell>
+                                        {imgObj.id}
+                                    </TableCell>
+                                    <TableCell>
+                                        <img style={{maxWidth: "200px", maxHeight: {maxRowHeight}}}
+                                             src={imgObj.thumbnail}
+                                             alt="Image Could Not be Load"
+                                             loading={"lazy"}/>
+                                    </TableCell>
+                                    <TableCell>
+                                        <p style={{maxHeight: {maxRowHeight}, overflow: "scroll"}}>{imgObj.title} </p>
+                                    </TableCell>
+                                    <TableCell>
+                                        <p style={{
+                                            maxHeight: {maxRowHeight},
+                                            overflow: "scroll"
+                                        }}> {imgObj.description} </p>
+                                    </TableCell>
+                                    <TableCell>
+                                        {imgObj.dateTaken}
+                                    </TableCell>
+                                    <TableCell>
+                                        {imgObj.ownerName}
+                                    </TableCell>
+                                    <TableCell>
+                                        {imgObj.latitude}
+                                    </TableCell>
+                                    <TableCell>
+                                        {imgObj.longitude}
+                                    </TableCell>
+                                    <TableCell>
+                                        {imgObj.views}
+                                    </TableCell>
+                                </TableRow>
+
+                            </>
+                        )
+                    })}
+                </Table>
+            </div>
         </>
     )
 }
+
 export default FlickrPhotosListResult;
